0d703dcc1b0e3b750b96dd6c74a6038af94c57c3
mohankumaraspire <mohankumar.sakthivel@aspiresys.com>
2023-05-17 20:19:34 +0530

    Observables 17

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 
 export class RecordsService {
-  info1: any[] = ['Mohankumar', '3499', 'mohana@aspire.ney']
+  info1: any[] = ['Mohankumar', '3499', 'mohan@aspire.net'];
+  info2: any[] = ['Rameshkumar', '1234', 'ramesh@aspire.net'];
+  info3: any[] = ['Sathish', '5678', 'sathish@aspire.net'];
   constructor() { }
 }
